2bbd6251e76b783efbd99209611fa391d280f2bd
Mabuljo <151923541+Mabuljo@users.noreply.github.com>
2024-04-22 18:32:39 +0200

    ajout du fichier script.js

--- assets/script.js
+++ assets/script.js
@@ -16,19 +16,36 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-/*Ajout de la constante des flèches */
+
+// tableau slides [0,1,2,3]
+
+// Ajout de la constante des flèches
 const arrows = document.querySelectorAll(".arrow");
 
-/* Ajout de la constante Slide active = slide visible */
+// Ajout de la constante Slide active => slide visible
 const slideActive = document.querySelector(".active");
 
 
-/* Pour chaque flèche écouter le clic */
+// Pour chaque flèche écouter le clic
 arrows.forEach((arrow) => {
 	arrow.addEventListener('click', (e) => {
-	
+		// si je clique sur arrow_left je fais -1 et si je clique sur arrow_right je fais +1
+		//calcNextSlide => est ce que e.target.classlist[1] est égal à arrow_right? si oui ca renvoie +1 si non renvoie -1
+		const calcNextSlide = e.target.classList[1] === "arrow_right" ? 1 : -1;
+
+		//newIndex => calcule où est la class active dans le tableau slides 
+		//si calcNextSlide=-1 et ([...slides].indexOf(slideActive))=1 on aura 0 donc la 1e image
+		newIndex = calcNextSlide + ([...slides].indexOf(slideActive));
+
+		//Pour avoir le dernier élément du tableau 
+		if(newIndex < 0) {newIndex = slides.length -1};
+
+		//pour avoir le 1e élément du tableau
+		if(newIndex >= slides.length) {newIndex = 0;};
+
+		//pour ajouter la class active au nouvel index du tableau et qui permet de montrer l'image à l'écran
+		slides[newIndex].classList.add("active");
+		//pour enlever la class active à SlideActive
+		slideActive.classList.remove("active");
 	})
 });
-
-
-
